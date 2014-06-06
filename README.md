298901752489b4cb646693b9766ebbb2e497d088
rayjune <rayjunewu@gmail.com>
2014-06-06 11:33:21 +0800

    alter README.md

--- README.md
+++ README.md
@@ -2,3 +2,34 @@ RJBlurAlertView
 ===============
 
 RJBlurAlertView is a custom alert view with a blurred background view.It can be showed as dropping or bounce.
+
+
+#How To Use
+**Ensure that you has added `GPUImage.framework` in your project.**
+
+RJBlurAlertView has `RJBlurAlertViewTypeText` and `RJBlurAlertViewTypeCustomView` types.You can use it following ways:
+
+Title and Text:
+--------------
+	RJBlurAlertView *alertView = [[RJBlurAlertView alloc] initWithTitle:@"title" text:@"this is text" cancelButton:YES color:[UIColor blueColor]];
+    
+    //set animation type
+    alertView.animationType = RJBlurAlertViewAnimationTypeDrop;
+    
+    [alertView setCompletionBlock:^(RJBlurAlertView *alert, UIButton *button) {
+        if (button == alert.okButton) {
+            NSLog(@"ok button touched!");
+        }else{
+            NSLog(@"cancel button touched!");
+        }
+    }];
+    [alertView show];
+    
+Custom content view:
+-------------------
+
+	UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 180, 180)]; 
+	contentView.backgroundColor = [UIColor blackColor];
+    RJBlurAlertView *alertView = [[RJBlurAlertView alloc] initWithTitle:@"title" contentView:contentView cancelButton:YES color:[UIColor blueColor]];
+    [alertView show];
+
--- RJBlurAlertView/RJBlurAlertView/RJViewController.m
+++ RJBlurAlertView/RJBlurAlertView/RJViewController.m
@@ -21,23 +21,23 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 - (IBAction)showAlertViewTouch:(UIButton *)sender {
-//    RJBlurAlertView *alertView = [[RJBlurAlertView alloc] initWithTitle:@"title" text:@"this is text" cancelButton:YES color:[UIColor blueColor]];
-//    if (sender.tag == 1) {
-//        alertView.animationType = RJBlurAlertViewAnimationTypeDrop;
-//    }
-//    [alertView setCompletionBlock:^(RJBlurAlertView *alert, UIButton *button) {
-//        if (button == alert.okButton) {
-//            NSLog(@"ok button touched!");
-//        }else{
-//            NSLog(@"cancel button touched!");
-//        }
-//    }];
-//    [alertView show];
-    
-    UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 180, 180)];
-    contentView.backgroundColor = [UIColor blackColor];
-    RJBlurAlertView *alertView = [[RJBlurAlertView alloc] initWithTitle:@"title" contentView:contentView cancelButton:YES color:[UIColor blueColor]];
+    RJBlurAlertView *alertView = [[RJBlurAlertView alloc] initWithTitle:@"title" text:@"this is text" cancelButton:YES color:[UIColor blueColor]];
+    if (sender.tag == 1) {
+        alertView.animationType = RJBlurAlertViewAnimationTypeDrop;
+    }
+    [alertView setCompletionBlock:^(RJBlurAlertView *alert, UIButton *button) {
+        if (button == alert.okButton) {
+            NSLog(@"ok button touched!");
+        }else{
+            NSLog(@"cancel button touched!");
+        }
+    }];
     [alertView show];
+    
+//    UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 180, 180)];
+//    contentView.backgroundColor = [UIColor blackColor];
+//    RJBlurAlertView *alertView = [[RJBlurAlertView alloc] initWithTitle:@"title" contentView:contentView cancelButton:YES color:[UIColor blueColor]];
+//    [alertView show];
 }
 
 - (void)didReceiveMemoryWarning
